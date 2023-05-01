b2892058cb7f625c4e329b873cff5502e327bc9a
Makhlin.Borys <Makhlin.Borys@scheidt-bachmann.de>
2023-05-01 23:21:49 +0300

    Add skeleton

--- src/components/AudioPanel/CurrentlyPlayingTrack.js
+++ src/components/AudioPanel/CurrentlyPlayingTrack.js
@@ -1,17 +1,37 @@
-import { Stack, Typography } from "@mui/material";
+import { Skeleton, Stack, Typography } from "@mui/material";
 import React from "react";
 
 function CurrentlyPlayingTrack({ track }) {
   return (
     <Stack flex={1} direction="row" gap={2} alignItems="center">
-      <img
-        height={56}
-        src={track?.albumImage}
-        alt={track?.name ?? "album"}
-        loading="lazy"
-        style={{ borderRadius: "2px" }}
-      />
-      <Typography variant="span">{track?.name}</Typography>
+      {!track && (
+        <>
+          <Skeleton
+            variant="rectangular"
+            animation={false}
+            width={56}
+            height={56}
+          />
+          <Skeleton 
+            variant="rectangular"
+            animation={false}
+            width="35%"
+            height={10}
+          />
+        </>
+      )}
+      {track && (
+        <>
+          <img
+            height={56}
+            src={track?.albumImage}
+            alt={track?.name ?? "album"}
+            loading="lazy"
+            style={{ borderRadius: "2px" }}
+          />
+          <Typography variant="span">{track?.name}</Typography>
+        </>
+      )}
     </Stack>
   );
 }
